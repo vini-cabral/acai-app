150d46c23aedb90e48b0c0b308660aa6f4e21cbb
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2022-09-19 20:08:51 -0300

    Update env

--- src/client/env/index.ts
+++ src/client/env/index.ts
@@ -1,6 +1,5 @@
 const API_CLIENT = {
-  // baseURL: "https://acai-app.vercel.app/api",
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://acai-app.vercel.app/api",
 }
 
 export { API_CLIENT }
